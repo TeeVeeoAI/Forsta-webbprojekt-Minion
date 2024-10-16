6df8822ba772a38c00543d32a722222ffca015a6
TeeVeeoAI <tosi07001@utb.vaxjo.se>
2024-10-16 09:32:05 +0200

    IMG byte på Otto

--- minions.js
+++ minions.js
@@ -1,17 +1,10 @@
-function Onclick() {
-    document.getElementsByClassName("text").style.backgroundColor= "black";
-    document.getElementByClassName("text").style.color = "white";
-    document.getElementByClassName("text").style.textShadow = "5px 3px 10px #ffffff2f";
-    document.getElementByClassName("text").style.borderRadius = "10px";
-    document.getElementById("onclick").style.visibility = "hidden";
-    document.getElementById("offclick").style.visibility = "visible"
-}
-
-function Offclick() {
-    document.getElementById("text").style.backgroundColor = "";
-    document.getElementById("text").style.color = "";
-    document.getElementById("text").style.textShadow = "";
-    document.getElementById("text").style.borderRadius = "";
-    document.getElementById("offclick").style.visibility = "hidden";
-    document.getElementById("onclick").style.visibility = "visible"
+let i = 1;
+function IMGOtto() {
+    if (i == 1) {
+        document.getElementById("img").src="Minions img/Otto/Otto2.jpg";
+        i++;
+    } else if (i == 2) {
+        document.getElementById("img").src="Minions img/Otto/Otto.png";
+        i--;
+    }
 }
