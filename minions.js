322284073d9868093270e2a75dcdf45e519838fd
TeeVeeoAI <tosi07001@utb.vaxjo.se>
2024-10-18 13:06:46 +0200

    +

--- minions.js
+++ minions.js
@@ -1,31 +1,31 @@
 let otto = 1, jerry = 1, mel = 1, bob = 1, carl = 1;
 function IMGOtto() {
     if (otto == 1) {
-        document.getElementById("Otto").src="Minions img/Otto/Otto2.jpg";
+        IMG("Otto", "Minions img/Otto/Otto2.jpg");
         otto++;
     } else if (otto == 2) {
-        document.getElementById("Otto").src="Minions img/Otto/Otto.png";
+        IMG("Otto", "Minions img/Otto/Otto.png");
         otto--;
     }
 }
 
 function IMGJerry() {
     if (jerry == 1) {
-        document.getElementById("Jerry").src="Minions img/Jerry/Jerry_Evil.png.webp";
+        IMG("Jerry", "Minions img/Jerry/Jerry_Evil.png.webp");
         jerry++;
     } else if (jerry == 2) {
-        document.getElementById("Jerry").src="Minions img/Jerry/Jerry.webp";
+        IMG("Jerry", "Minions img/Jerry/Jerry.webp");
         jerry--;
     }
 }
 
 function IMGMel() {
     if (mel == 1) {
-        document.getElementById("Mel").src="Minions img/Mel/Mel2.webp";
+        IMG("Mel", "Minions img/Mel/Mel2.webp");
         mel++;
         Width("Mel", "100%");
     } else if (mel == 2) {
-        document.getElementById("Mel").src="Minions img/Mel/Mel.webp";
+        IMG("Mel", "Minions img/Mel/Mel.webp");
         mel--;
         Width("Mel", "50%");
     }
@@ -33,20 +33,20 @@ function IMGMel() {
 
 function IMGBob() {
     if (bob == 1) {
-        document.getElementById("Bob").src="Minions img/Bob/Bob2.png";
+        IMG("Bob", "Minions img/Bob/Bob2.png");
         bob++;
     } else if (bob == 2) {
-        document.getElementById("Bob").src="Minions img/Bob/Bob.png";
+        IMG("Bob", "Minions img/Bob/Bob.png");
         bob--;
     }
 }
 
 function IMGCarl() {
     if (carl == 1) {
-        document.getElementById("Carl").src="Minions img/Carl/Carl_bee-do.webp";
+        IMG("Carl", "Minions img/Carl/Carl_bee-do.webp");
         carl++;
     } else if (carl == 2) {
-        document.getElementById("Carl").src="Minions img/Carl/Carl.png";
+        IMG("Carl", "Minions img/Carl/Carl.png");
         carl--;
     }
 }
@@ -56,5 +56,5 @@ function Width(id, size) {
 }
 
 function IMG(id, src) {
-
+    document.getElementById(id).src = src;
 }
