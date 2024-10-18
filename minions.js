bdb01bf7381d5509eea178ec591401abafd8e48e
TeeVeeoAI <tosi07001@utb.vaxjo.se>
2024-10-18 09:37:38 +0200

    +Mel lore

--- minions.js
+++ minions.js
@@ -1,10 +1,36 @@
-let i = 1;
+let i = 1, j = 1, l = 1;
 function IMGOtto() {
     if (i == 1) {
-        document.getElementById("img").src="Minions img/Otto/Otto2.jpg";
+        document.getElementById("Otto").src="Minions img/Otto/Otto2.jpg";
         i++;
     } else if (i == 2) {
-        document.getElementById("img").src="Minions img/Otto/Otto.png";
+        document.getElementById("Otto").src="Minions img/Otto/Otto.png";
         i--;
     }
+}
+
+function IMGJerry() {
+    if (j == 1) {
+        document.getElementById("Jerry").src="Minions img/Jerry/Jerry_Evil.png.webp";
+        j++;
+    } else if (j == 2) {
+        document.getElementById("Jerry").src="Minions img/Jerry/Jerry.webp";
+        j--;
+    }
+}
+
+function IMGMel() {
+    if (l == 1) {
+        document.getElementById("Mel").src="Minions img/Mel/Mel2.webp";
+        l++;
+        Width("Mel", "100%");
+    } else if (l == 2) {
+        document.getElementById("Mel").src="Minions img/Mel/Mel.webp";
+        l--;
+        Width("Mel", "50%");
+    }
+}
+
+function Width(a, b) {
+    document.getElementById(a).style.width = b;
 }
